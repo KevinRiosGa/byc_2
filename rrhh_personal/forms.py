352155f8e0a0b270fef1e5e2030adeb40e477679
GDiazF <memodiazflores@gmail.com>
2025-02-27 22:42:08 -0300

    actualizacion de formulario de ingreso de personal

--- rrhh_personal/forms.py
+++ rrhh_personal/forms.py
@@ -5,10 +5,41 @@ from .models import *
 #formulario para la creacion de personas
 class PersonalCreationForm(forms.ModelForm):
 
+    sexo_id = forms.ModelChoiceField(
+        queryset=Sexo.objects.all(),
+        empty_label='-----------',
+        widget=forms.Select(attrs={'class': 'form-select'})  # Aquí se aplica el widget
+    )
+    
+    region_id = forms.ModelChoiceField(
+        queryset=Region.objects.all(),
+        empty_label='-----------',
+        widget=forms.Select(attrs={'class': 'form-select'})  # Aquí también
+    )
+
+    comuna_id = forms.ModelChoiceField(
+        queryset=Comuna.objects.all(),
+        empty_label='-----------',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+
+    estcivil_id = forms.ModelChoiceField(
+        queryset=EstadoCivil.objects.all(),
+        empty_label='-----------',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+
+
     class Meta:
         model = Personal
         fields = ['rut', 'dvrut', 'nombre', 'apepat', 'apemat', 'sexo_id', 'fechanac', 'estcivil_id', 'correo', 'region_id', 'comuna_id', 'direccion']
         widgets = {
+            'rut': forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}),
+            'dvrut': forms.TextInput(attrs={'type': 'text', 'class': 'form-control', 'id': 'id_dvrut', 'readonly': 'readonly'}),
+            'nombre': forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}),
+            'apepat': forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}),
+            'apemat': forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}),
+            'direccion': forms.TextInput(attrs={'type': 'text', 'class': 'form-control'}),
             'fechanac' : forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
             'correo': forms.EmailInput(attrs={'type': 'email', 'class': 'form-control'}),
         }
@@ -27,44 +58,37 @@ class PersonalCreationForm(forms.ModelForm):
             'sexo_id':'Sexo',
         }
 
-    sexo_id = forms.ModelChoiceField(queryset=Sexo.objects.all(), empty_label= '-----------', widget=forms.Select(attrs={'class': 'form-select'}))
-    region_id = forms.ModelChoiceField(queryset=Region.objects.all(), empty_label='-----------', widget=forms.Select(attrs={'class': 'form-select'}))
-    comuna_id = forms.ModelChoiceField(queryset=Comuna.objects.all(), empty_label='-----------', widget=forms.Select(attrs={'class': 'form-select'}))
-    estcivil_id = forms.ModelChoiceField(queryset=EstadoCivil.objects.all(), empty_label='-----------', widget=forms.Select(attrs={'class': 'form-select'}))
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Actualizando los widgets para rut y dvrut
-        self.fields['rut'].widget.attrs.update({'class': 'form-control', 'id': 'id_rut'})
-        self.fields['dvrut'].widget.attrs.update({'class': 'form-control', 'id': 'id_dvrut', 'readonly': 'readonly'})
-        self.fields['nombre'].widget.attrs.update({'class': 'form-control'})
-        self.fields['apepat'].widget.attrs.update({'class': 'form-control'})
-        self.fields['apemat'].widget.attrs.update({'class': 'form-control'})
-
-        # Personalización de campos select
-        self.fields['sexo_id'].widget.attrs.update({'class': 'form-select', 'aria-label': 'Sexo'})
-        self.fields['region_id'].widget.attrs.update({'class': 'form-select', 'aria-label': 'Región'})
-        self.fields['comuna_id'].widget.attrs.update({'class': 'form-select', 'aria-label': 'Comuna'})
-        self.fields['estcivil_id'].widget.attrs.update({'class': 'form-select', 'aria-label': 'Estado civil'})
-        self.fields['fechanac'].widget.attrs.update({'class': 'form-control', 'aria-label': 'Fecha de nacimiento'})
-        self.fields['correo'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Correo electrónico'})
+    
 
 #formulario para ingresar la informacion laboral de las personas
 class InfoLaboralPersonalForm(forms.ModelForm):
+
+    empresa_id = forms.ModelChoiceField(
+        queryset=Empresa.objects.all(),
+        empty_label='-----------',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+
+    depto_id = forms.ModelChoiceField(
+        queryset=DeptoEmpresa.objects.all(),
+        empty_label='-----------',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
+
+    cargo_id = forms.ModelChoiceField(
+        queryset=Cargo.objects.all(),
+        empty_label='-----------',
+        widget=forms.Select(attrs={'class': 'form-select'})
+    )
 
     class Meta:
         model = InfoLaboral
         fields = ['empresa_id', 'depto_id', 'cargo_id', 'fechacontrata']
         widgets = {
-            'fechacontrata' : forms.DateInput(attrs={'type': 'date'}),
+            'fechacontrata' : forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
         }
 
         labels = {'fechacontrata' : 'Fecha Contrata'}
-
-    empresa_id = forms.ModelChoiceField(queryset=Empresa.objects.all(), empty_label='-----------')
-    depto_id = forms.ModelChoiceField(queryset=DeptoEmpresa.objects.all(),empty_label='-----------')
-    cargo_id = forms.ModelChoiceField(queryset=Cargo.objects.all(),empty_label='-----------')
-
 
 #formularios de licencias --------------------------------------------------------------
 class LicenciasPersonal(forms.ModelForm):
